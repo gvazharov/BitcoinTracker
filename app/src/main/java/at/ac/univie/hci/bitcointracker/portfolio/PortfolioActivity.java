7ca9913f5ce1a97f22825005e9bc73bff51eeb60
Gavril Vazharov <g.vujarov@gmail.com>
2018-05-05 09:23:05 +0200

    add FeeFragment on startup PortfolioActivity

--- app/src/main/java/at/ac/univie/hci/bitcointracker/portfolio/PortfolioActivity.java
+++ app/src/main/java/at/ac/univie/hci/bitcointracker/portfolio/PortfolioActivity.java
@@ -30,7 +30,7 @@ public class PortfolioActivity extends FragmentActivity {
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.container_portfolio, new FeesFragment())
-                .add(R.id.container_portfolio, new CoinPriceFragment())
+//                .add(R.id.container_portfolio, new CoinPriceFragment())
                 .commit();
 
     }
