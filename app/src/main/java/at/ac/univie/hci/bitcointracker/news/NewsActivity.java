76b4355511fe507e4aa1d119f010c2c804e11f7e
Eric <38755444+TheWise0wl@users.noreply.github.com>
2018-05-11 07:13:57 +0200

    Add files via upload

---
+++ app/src/main/java/at/ac/univie/hci/bitcointracker/news/NewsActivity.java
@@ -0,0 +1,24 @@
+package at.ac.univie.hci.bitcointracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class NewsActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        //ruft RSS feed auf
+        RSSparser readRss = new RSSparser(this, recyclerView);
+        readRss.execute();
+    }
+}
