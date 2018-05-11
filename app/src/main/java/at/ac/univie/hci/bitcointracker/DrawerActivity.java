f4093637dacef3d58d74c43c75c3d58cec2ed9f5
Gavril Vazharov <g.vujarov@gmail.com>
2018-05-11 17:35:00 +0200

    change Chart/CoinPrice onStartUp

--- app/src/main/java/at/ac/univie/hci/bitcointracker/DrawerActivity.java
+++ app/src/main/java/at/ac/univie/hci/bitcointracker/DrawerActivity.java
@@ -1,18 +1,24 @@
 package at.ac.univie.hci.bitcointracker;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.support.v4.app.Fragment;
+import at.ac.univie.hci.bitcointracker.portfolio.CoinPriceFragment;
+import at.ac.univie.hci.bitcointracker.portfolio.ManagePortfolioCoinsFragment;
 
 public class DrawerActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -80,13 +86,16 @@ public class DrawerActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+        Fragment fragment = null;
+        Class fragmentClass = null;
         if (id == R.id.portfolio_menu) {
-            Toast.makeText(getApplicationContext(), "PORTFOLIO", Toast.LENGTH_SHORT).show();
+            fragmentClass = CoinPriceFragment.class;
+;        }
+        else if (id == R.id.settings_menu) {
+            Intent intent = new Intent(DrawerActivity.this, SettingsActivity.class );
+            startActivity(intent);
         }
-//        else if (id == R.id.nav_gallery) {
-//
-//        } else if (id == R.id.nav_slideshow) {
+//        else if (id == R.id.nav_slideshow) {
 //
 //        } else if (id == R.id.nav_manage) {
 //
@@ -95,6 +104,14 @@ public class DrawerActivity extends AppCompatActivity
 //        } else if (id == R.id.nav_send) {
 //
 //        }
+        try{
+            fragment = (Fragment) fragmentClass.newInstance();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.container_portfolio, fragment).commit();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
